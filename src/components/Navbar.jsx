cc9908ef2fbcdd7156f9f418a67335f6a2608950
Prakhar Pandey <prakhar10may@gmail.com>
2023-05-11 10:58:20 +0530

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,9 +11,6 @@ const Navbar = () => {
             <li className='px-6'> <Link to="skill" spy={true} smooth={true} offset={270} duration={500} className='cursor-pointer text-lg lg:text-xl '>Skills</Link> </li>
             <li className='px-6'><Link to="project" spy={true} smooth={true} offset={100} duration={1000} className='cursor-pointer text-lg lg:text-xl '>Projects</Link></li>
             <li className='px-6'><Link to="connect" spy={true} smooth={true} offset={200} duration={1000} className='cursor-pointer text-lg lg:text-xl '>Contact</Link></li>
-            <li>
-              <a className='border-2 border-b-white bg-gradient-to-r from-cyan-500 to-teal-700 text-white mx-5 px-4 py-2 rounded-xl ml-8 mr-10 text-lg' href="https://drive.google.com/file/d/1szvgC5uXbuJZ5UmzXgADdB0q1S8hMGO4/view">Resume</a>
-            </li>
           </ul>
         </nav> 
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,9 @@ import netflix from "../images/netflix.png"
 import youtube from '../images/youtube.png'
 import dailyburn from '../images/dailyburn.png'
 import enotebook from '../images/enotebook.png'
+import chitchat from '../images/chitchat.png'
+import cointrax from '../images/cointrax.png'
+
 const Projects = () => {
   return (
     <div className='px-10' id='project'>
@@ -12,26 +15,61 @@ const Projects = () => {
 
         <h3 className='sm:text-lg md:test-xl lg:text-2xl md:py-6 sm:py-3 font-burtons '>Checkout some of my work . . . . . . . . .</h3>
 
-
-
-
         <div className='sm:flex sm:flex-col sm:justify-center sm:align-middle sm:items-center md:grid md:grid-cols-2 md:gap-4 md:justify-center '>
 
 
             <div className="max-w-xl bg-[#BAD7E9] border-2 shadow-2xl shadow-cyan-500/70 rounded-lg hover:scale-105 ease-in duration-200 border-white dark:bg-gray-800 dark:border-gray-700 md:ml-20 mb-16">
-                <a href="https://daily-burn.netlify.app/">
+                <a href="https://cointrax.netlify.app/" target={'blank'}>
+                    <img className="rounded-t-lg object-contain" src={cointrax} alt="" />
+                </a>
+                <div className="p-5">
+                    <a href="https://cointrax.netlify.app/" target={'blank'}>
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">CoinTrax - Tracking the pulse of crypto market</h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">CoinTrax is a fully responsive website which provides latest data about cryptocurrencies as well as different exchanges.</p>
+                    <a href="https://github.com/prakhar-1005/CoinTrax" target={'blank'} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white border-2 border-b-white bg-gradient-to-r from-cyan-500 to-teal-700 rounded-xl mr-10">
+                        Code
+                        <i className="ml-2 fa-solid fa-code"></i>
+                    </a>
+                </div>
+            </div>
+
+
+
+            <div className="max-w-xl bg-[#BAD7E9] border-2 shadow-2xl shadow-cyan-500/70  hover:scale-105 ease-in duration-200 border-white rounded-lg dark:bg-gray-800 dark:border-gray-700 mb-16">
+                <a href="https://chit-chat-84266.web.app/" target={'blank'}>
+                    <img className="rounded-t-lg" src={chitchat} alt="" />
+                </a>
+                <div className="p-5">
+                    <a href="https://chit-chat-84266.web.app/" target={'blank'}>
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Chit-Chat - A real time chat application</h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">A realtime chat application made using ReactJS and Firebase.</p>
+                    <a href="https://github.com/prakhar-1005/Chit-Chat" target={'blank'} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white border-2 border-b-white bg-gradient-to-r from-cyan-500 to-teal-700 rounded-xl">
+                        Code
+                        <i className="ml-2 fa-solid fa-code"></i>
+                    </a>
+                </div>
+            </div>
+
+
+
+
+
+            <div className="max-w-xl bg-[#BAD7E9] border-2 shadow-2xl shadow-cyan-500/70 rounded-lg hover:scale-105 ease-in duration-200 border-white dark:bg-gray-800 dark:border-gray-700 md:ml-20 mb-16">
+                <a href="https://daily-burn.netlify.app/" target={'blank'}>
                     <img className="rounded-t-lg object-contain" src={dailyburn} alt="" />
                 </a>
                 <div className="p-5">
-                    <a href="https://daily-burn.netlify.app/">
+                    <a href="https://daily-burn.netlify.app/" target={'blank'}>
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">DailyBurn Fitness Web Application</h5>
                     </a>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">A MERN stack application for the people to note down the exercises they want to do.</p>
-                    <a href="https://github.com/prakhar-1005/DailyBurn_fitness_app_frontend" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white border-2 border-b-white bg-gradient-to-r from-cyan-500 to-teal-700 rounded-xl mr-10">
+                    <a href="https://github.com/prakhar-1005/DailyBurn_fitness_app_frontend" target={'blank'} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white border-2 border-b-white bg-gradient-to-r from-cyan-500 to-teal-700 rounded-xl mr-10">
                         Code (Frontend)
                         <i className="ml-2 fa-solid fa-code"></i>
                     </a>
-                    <a href="https://github.com/prakhar-1005/DailyBurn_fitness_app_backend" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white border-2 border-b-white bg-gradient-to-r from-cyan-500 to-teal-700 rounded-xl mr-10">
+                    <a href="https://github.com/prakhar-1005/DailyBurn_fitness_app_backend"  target={'blank'} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white border-2 border-b-white bg-gradient-to-r from-cyan-500 to-teal-700 rounded-xl mr-10">
                         Code (backend)
                         <i className="ml-2 fa-solid fa-code"></i>
                     </a>
@@ -39,15 +77,15 @@ const Projects = () => {
             </div>
 
             <div className="max-w-xl bg-[#BAD7E9] border-2 shadow-2xl shadow-cyan-500/70  hover:scale-105 ease-in duration-200 border-white rounded-lg dark:bg-gray-800 dark:border-gray-700 mb-16">
-                <a href="https://you-tube1.netlify.app/">
+                <a href="https://you-tube1.netlify.app/" target={'blank'}>
                     <img className="rounded-t-lg" src={youtube} alt="" />
                 </a>
                 <div className="p-5">
-                    <a href="https://you-tube1.netlify.app/">
+                    <a href="https://you-tube1.netlify.app/" target={'blank'}>
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">YouTube Web Application</h5>
                     </a>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">A fully responsive and working YouTube clone web application made using Reactjs and MUI.</p>
-                    <a href="https://github.com/prakhar-1005/youtube_app" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white border-2 border-b-white bg-gradient-to-r from-cyan-500 to-teal-700 rounded-xl">
+                    <a href="https://github.com/prakhar-1005/youtube_app" target={'blank'} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white border-2 border-b-white bg-gradient-to-r from-cyan-500 to-teal-700 rounded-xl">
                         Code
                         <i className="ml-2 fa-solid fa-code"></i>
                     </a>
@@ -56,15 +94,15 @@ const Projects = () => {
 
 
             <div className="max-w-xl bg-[#BAD7E9] border-2 shadow-2xl shadow-cyan-500/70  hover:scale-105 ease-in duration-200 border-white rounded-lg dark:bg-gray-800 dark:border-gray-700 md:ml-20 sm:mb-16 md:mb-0">
-                <a href="https://github.com/prakhar-1005/E-Notebook">
+                <a href="https://github.com/prakhar-1005/E-Notebook" target={'blank'}>
                     <img className="rounded-t-lg" src={enotebook} alt="" />
                 </a>
                 <div className="p-5">
-                    <a href="https://github.com/prakhar-1005/E-Notebook">
+                    <a href="https://github.com/prakhar-1005/E-Notebook" target={'blank'}>
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">E-Notebook - Your notes on the cloud</h5>
                     </a>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">A MERN stack application used for storing important notes securely on the cloud.</p>
-                    <a href="https://github.com/prakhar-1005/E-Notebook" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white border-2 border-b-white bg-gradient-to-r from-cyan-500 to-teal-700 rounded-xl">
+                    <a href="https://github.com/prakhar-1005/E-Notebook" target={'blank'} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white border-2 border-b-white bg-gradient-to-r from-cyan-500 to-teal-700 rounded-xl">
                         Code
                         <i className="ml-2 fa-solid fa-code"></i>
                     </a>
@@ -74,51 +112,21 @@ const Projects = () => {
 
 
             <div className="max-w-xl bg-[#BAD7E9] border-2 shadow-2xl shadow-cyan-500/70  hover:scale-105 ease-in duration-200 border-white rounded-lg  dark:bg-gray-800 dark:border-gray-700">
-                <a href="https://movieflix.netlify.app/">
+                <a href="https://movieflix.netlify.app/" target={'blank'}>
                     <img className="rounded-t-lg" src={netflix} alt="" />
                 </a>
                 <div className="p-5">
-                    <a href="https://movieflix.netlify.app/">
+                    <a href="https://movieflix.netlify.app/" target={'blank'}>
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">MovieFlix</h5>
                     </a>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">A NetFlix clone site made using react.js for creating the frontend and API for movie posters.</p>
-                    <a href="https://github.com/prakhar-1005/MovieFlix" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white border-2 border-b-white bg-gradient-to-r from-cyan-500 to-teal-700 rounded-xl">
+                    <a href="https://github.com/prakhar-1005/MovieFlix" target={'blank'} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white border-2 border-b-white bg-gradient-to-r from-cyan-500 to-teal-700 rounded-xl">
                         Code
                         <i className="ml-2 fa-solid fa-code"></i>
                     </a>
                 </div>
             </div>
 
-
-
-
-
-
-
-{/* 
-            <a href="https://github.com/prakhar-1005/MovieFlix" className='group'>
-            <div className='flex justify-center'>
-                <img src={netflix} className="group-hover:opacity-70 shadow-2xl w-[650px] h-[320px] my-5 rounded-xl hover:scale-105 ease-in duration-200 hover:blur-[0px] cursor-pointer"></img> 
-            </div>
-            </a>     */}
-
-            {/* <a href="https://github.com/prakhar-1005/youtube_app" className='group'>
-            <div className='flex justify-center'>
-                <img src={youtube} className="group-hover:opacity-70 shadow-2xl w-[650px] h-[320px] my-5 rounded-xl hover:scale-105 ease-in duration-200 hover:blur-[0px] cursor-pointer"></img> 
-            </div> 
-            </a>      
-
-            <a href="#" className='group'>
-            <div className='flex justify-center'>
-                <img src={tindog} className="group-hover:opacity-70 shadow-2xl w-[650px] h-[320px] my-5 rounded-xl hover:scale-105 ease-in duration-200 hover:blur-[0px] cursor-pointer"></img> 
-            </div>  
-            </a>      
-            
-            <a href="https://github.com/prakhar-1005/Chit-Chat" className='group'>
-            <div className='flex justify-center'>
-                <img src={chitchat} className="group-hover:opacity-70 shadow-2xl w-[650px] h-[320px] my-5 rounded-xl hover:scale-105 ease-in duration-200 hover:blur-[0px] cursor-pointer"></img> 
-            </div>
-            </a> */}
         </div>
 
     </div>
